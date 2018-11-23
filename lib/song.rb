2ef8106ebff25b23760f597a201d0d03947c3b04
Janine Luk <janineluk95@gmail.com>
2018-11-23 13:25:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,21 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name
+  end
 
+  
   def self.all
     @@all
   end
+
 
   def save
     self.class.all << self
